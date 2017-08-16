7f04a7162e3abbee59a2a3ecbbff214f8af85aad
hlpureboy <hlpureboy@163.com>
2017-08-16 20:51:05 +0800

    Update SqlDB.py

--- SqlDB.py
+++ SqlDB.py
@@ -4,7 +4,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 def insert(Time,Title,level,Author):
-    con=MySQLdb.connect(host='112.74.204.232',user='root',passwd='123pyj',db='edubug',charset='utf8')
+    con=MySQLdb.connect(host='127.0.0.1',user='root',passwd='root',db='edubug',charset='utf8')
     cursor=con.cursor()
     sql='insert into edubug(Time,Title,leve,Author) values (%s,%s,%s,%s)'
     parm=(Time,Title,level,Author)
